96dc2fe751ee1a0225c9fca3860da3882d3001dc
OMlalala <422173092@qq.com>
2017-03-24 22:40:00 +0800

    update

--- ex.py
+++ ex.py
@@ -1,34 +1,8 @@
 # -*- coding: utf-8 -*-  
 
-# # import Flask from the flask library
-# from flask import Flask, render_template, request
-#
-# # Create a new Flask instance as a variable named app.
-# # The name passed to the Flask app should be __name__.
-# app = Flask(__name__)
-#
-# @app.route('/') # give the view a route of '/'
-# def index(): # add a view function named index
-#     return render_template('index.html') # make the view function return
-#
-#
-# @app.route('/search/', methods=['GET', 'POST'])
-#
-# # def get_movie(user_input):
-# #     r = requests.get('http://api.douban.com/v2/movie/search?q={user_input}')
-#
-# def search():
-#     r = request.get('http://api.douban.com/v2/movie/search?q={rating}')
-#     r_dict = r.json()
-#     return render_template('search.html', rating=r_dict['subjects'][0]['rating']['average'])
-#
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-
 import requests
 
-user_input = raw_input("请输入要查询的电影： ")
+user_input = raw_input("请输入要查询的电影名称： ")
 
 url = 'http://api.douban.com/v2/movie/search?q={%s}' % user_input
 r = requests.get(url)
@@ -44,4 +18,8 @@ r2_dict = r2.json()
 
 ratings_count = r2_dict['ratings_count']
 
-print rating, ratings_count, id, images
+print """
+电影评分：%r
+评分人数：%d
+电影海报：%r
+""" % (rating, ratings_count, images)
